9fa6791e6b032b852715b2b03c677425cf79969c
JacksonMossman <54289017+JacksonMossman@users.noreply.github.com>
2020-07-10 18:14:23 -0500

    StartBranch

---
+++ Assets/AbilityMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AbilityMenu : MonoBehaviour
+{
+    public GameObject[] Abilities;
+    int selectedAbility = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(Abilities[selectedAbility]);
+    }
+}
---
+++ Assets/CharacterControllerBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerBehavior : MonoBehaviour
+{
+    public float speed;
+    public float Accel;
+    bool isMoving;
+    Vector3 Target;
+    public bool AccelerationBody;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(AccelerationBody)
+        {
+            AcceleratingBody();
+        }
+        
+       if(Target != null)
+       {
+            MoveToTarget(Target);
+       }
+
+    }
+    private void MoveToTarget(Vector3 TargetPos)
+    {
+        var step = speed * Time.deltaTime;
+
+        Vector3.MoveTowards(transform.position, TargetPos, step);
+    }
+   private void AcceleratingBody()
+   {
+        speed = speed + Accel * Time.deltaTime;
+   }
+
+}
