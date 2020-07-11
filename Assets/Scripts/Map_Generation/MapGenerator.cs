4fc73f544d01fb6df9119f671b1fa947227473f3
johnCavatelli <41810615+johnCavatelli@users.noreply.github.com>
2020-07-11 16:12:00 -0500

    Navigation improvements and improving the wall tiles

--- Assets/Scripts/Map_Generation/MapGenerator.cs
+++ Assets/Scripts/Map_Generation/MapGenerator.cs
@@ -7,14 +7,24 @@ using UnityEngine.Tilemaps;
 public class MapGenerator : MonoBehaviour
 {
     public Tilemap tilemap;//the tilemap to be edited
+    public Tilemap wallTilemap;
+    public Tilemap interactableTilemap;
 
     public TileBase groundTile;
     public TileBase wallTile;
     public TileBase topTile;
+    public TileBase randomWallTile;
+    public TileBase torchTile;
+    public GameObject lever;
+   
     public Transform playerTransform;
     public int size;//the size of the map (its a square)
     public int deadEnds = 5;//how many dead ends to generate
     public int width = 2;
+    public int wallFrequency;//how often to place a random wall thing
+    public int numDistractions;
+    public int numHazards;
+    public int numSwitches;
 
     //important locations
     Vector2Int startPoint;
@@ -22,8 +32,8 @@ public class MapGenerator : MonoBehaviour
     List<Vector2Int> filledPoints = new List<Vector2Int>();
     // Start is called before the first frame update
     void Start()
-    {
-        tilemap.BoxFill(new Vector3Int(0, 0, 0), topTile, 0, 0, size, size);
+    {       
+        wallTilemap.BoxFill(new Vector3Int(0, 0, 0), topTile, 0, 0, size, size);
           //finding start and end points
         float dist = 0;//distance between start and end point
         while (dist < size / 1.5f) {//find endpoints that are kinda far apart
@@ -33,7 +43,7 @@ public class MapGenerator : MonoBehaviour
         }
         generatePath(startPoint, endPoint);//generate the path from the begining to end
         playerTransform.position = tilemap.CellToWorld(new Vector3Int(startPoint.x, startPoint.y, 0));
-        
+        Debug.Log(tilemap.CellToWorld(new Vector3Int(startPoint.x, startPoint.y, 0)));
         //placing side paths with dead ends
         for(int i = 0; i < deadEnds; i++)
         {
@@ -41,13 +51,68 @@ public class MapGenerator : MonoBehaviour
         }
 
         //add edges
+        int j = 0;
         foreach(Vector2Int point in filledPoints){//for every point
             if(! filledPoints.Contains(point + new Vector2Int(0, 1)))//if there is not a filled point directly above it
             {
-                tilemap.SetTile(new Vector3Int(point.x, point.y + 1, 0), wallTile);
+                j++;
+                if(j == wallFrequency)//if it hit the iteration, set it to a either a random tile or a torch
+                {
+                    j = 0;
+                    TileBase tile = (UnityEngine.Random.Range(0, 2) >= 1) ? randomWallTile : torchTile;
+                    tilemap.SetTile(new Vector3Int(point.x, point.y + 1, 0), tile);
+                    wallTilemap.SetTile(new Vector3Int(point.x, point.y + 1, 0), null);
+                }
+                else
+                    tilemap.SetTile(new Vector3Int(point.x, point.y + 1, 0), wallTile);
+                    wallTilemap.SetTile(new Vector3Int(point.x, point.y + 1, 0), null);
             }
         }
          
+        //add distractions
+        for(int d = 0; d < numDistractions; d++)
+        {
+
+        }
+
+        //add hazards
+        for(int h = 0; h < numHazards; h++)
+        {
+
+        }
+        //add switches
+        for(int s = 0; s < numSwitches;s++)
+        {
+            int u = 0;
+            bool placed = false;//while it isn't placed
+            while (!placed && u<100)
+            {
+                u++;
+                Debug.Log("trying");
+                Vector2Int tryPosition = filledPoints[UnityEngine.Random.Range(0, filledPoints.Count - 1)];//pick a random tile
+                if (isAccessable(tryPosition))//if it's accessable
+                {
+                    Vector3 worldPos = tilemap.GetCellCenterWorld(new Vector3Int(tryPosition.x, tryPosition.y, 0));
+                    Instantiate(lever, worldPos, Quaternion.identity);
+                    Debug.Log("Placed lever");
+                    placed = true;
+                }
+            }
+            //= tilemap.CellToWorld()
+        }
+        //add exit door
+    }
+
+    private bool isAccessable(Vector2Int tryPosition)
+    {
+        if(tilemap.GetTile(new Vector3Int(tryPosition.x+1, tryPosition.y, 0)) != null && tilemap.GetTile(new Vector3Int(tryPosition.x-1, tryPosition.y, 0)) != null)
+        {
+            if (tilemap.GetTile(new Vector3Int(tryPosition.x , tryPosition.y+1, 0)) != null && tilemap.GetTile(new Vector3Int(tryPosition.x, tryPosition.y-1, 0)) != null)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     void generatePath(Vector2Int start, Vector2Int end)
@@ -77,8 +142,18 @@ public class MapGenerator : MonoBehaviour
                     tilemap.SetTile(new Vector3Int(tryPoint.x , tryPoint.y-i, 0), groundTile);//fill the grid
                     tilemap.SetTile(new Vector3Int(tryPoint.x - i, tryPoint.y-i, 0), groundTile);//fill the grid
                     tilemap.SetTile(new Vector3Int(tryPoint.x - i, tryPoint.y+i, 0), groundTile);//fill the grid
-                    
-                    currPoint = tryPoint;
+
+                    wallTilemap.SetTile(new Vector3Int(tryPoint.x, tryPoint.y, 0), null);//fill the grid
+                    wallTilemap.SetTile(new Vector3Int(tryPoint.x + i, tryPoint.y, 0), null);//fill the grid
+                    wallTilemap.SetTile(new Vector3Int(tryPoint.x - i, tryPoint.y, 0), null);//fill the grid
+                    wallTilemap.SetTile(new Vector3Int(tryPoint.x + i, tryPoint.y + i, 0), null);//fill the grid
+                    wallTilemap.SetTile(new Vector3Int(tryPoint.x + i, tryPoint.y - i, 0), null);//fill the grid
+                    wallTilemap.SetTile(new Vector3Int(tryPoint.x, tryPoint.y + i, 0), null);//fill the grid
+                    wallTilemap.SetTile(new Vector3Int(tryPoint.x, tryPoint.y - i, 0), null);//fill the grid
+                    wallTilemap.SetTile(new Vector3Int(tryPoint.x - i, tryPoint.y - i, 0), null);//fill the grid
+                    wallTilemap.SetTile(new Vector3Int(tryPoint.x - i, tryPoint.y + i, 0), null);//fill the grid
+
+                        currPoint = tryPoint;
                     //add it to the list of filled points                       
                     filledPoints.Add(currPoint + new Vector2Int(0,0));
                     filledPoints.Add(currPoint + new Vector2Int(1,0));
