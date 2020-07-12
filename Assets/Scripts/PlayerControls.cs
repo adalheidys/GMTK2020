294ebb58b97205f104eb0401f1bfff4e640a622c
Alice Hezketh Melesko <marlenaw@protonmail.com>
2020-07-12 17:17:20 +0300

    basic anim for the player

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,9 +6,12 @@ public class PlayerControls : MonoBehaviour
 {
     Rigidbody body;
     public float speed;
+    Animator animator;
+
     void Start()
     {
         body = transform.GetComponentInChildren<Rigidbody>();
+        animator = transform.GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -16,23 +19,28 @@ public class PlayerControls : MonoBehaviour
     {
         float horz = 0;
         float vert = 0;
+        animator.SetBool("inMotion", false);
         
         if ( (Input.GetKey("a") || Input.GetKey("left"))  )
         {
             horz = (-1) * speed * Time.fixedDeltaTime;
+            animator.SetBool("inMotion", true);
         }
         else if ( (Input.GetKey("d") || Input.GetKey("right"))  )
         {
             horz = (1) * speed * Time.fixedDeltaTime;
+            animator.SetBool("inMotion", true);
         }
         
         if ( (Input.GetKey("w") || Input.GetKey("up")) )
         {
             vert = (1) * speed * Time.fixedDeltaTime;
+            animator.SetBool("inMotion", true);
         }
         else if ( (Input.GetKey("s") || Input.GetKey("down"))  )
         {
             vert = (-1) * speed * Time.fixedDeltaTime;
+            animator.SetBool("inMotion", true);
         }
 
         body.velocity = new Vector3(horz, 0,vert) * speed * Time.fixedDeltaTime;
